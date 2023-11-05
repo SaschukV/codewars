269681d8ee69c7db407ce72ad11ec7a1f03e9fbb
SaschukV <v.saschuk@gmail.com>
2023-11-06 01:28:10 +0200

    positiveSum-second-solution

--- 8kyu/positiveSum.js
+++ 8kyu/positiveSum.js
@@ -9,3 +9,16 @@
 function positiveSum(arr) {
   return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0)
 }
+//✅Second Solution
+function positiveSum2(arr) {
+  let sum = 0
+  arr.forEach((el) => {
+    if (el > 0) {
+      sum += el
+    }
+  })
+  return sum
+}
+
+console.log('positiveSum:', positiveSum([-1, -2, -5, 0, -9, 6, -8])) // 6
+console.log('positiveSum2:', positiveSum2([-1, -2, -5, 0, -9, -6, -8])) //0
