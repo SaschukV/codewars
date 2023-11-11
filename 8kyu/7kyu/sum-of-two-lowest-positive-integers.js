7baa4cadc3dafa68775c1901ac04beee46d749f9
SaschukV <v.saschuk@gmail.com>
2023-11-11 19:23:58 +0200

    sumTwoSmallestNumbers

---
+++ 8kyu/7kyu/sum-of-two-lowest-positive-integers.js
@@ -0,0 +1,18 @@
+// ❓DESCRIPTION:
+// Create a function that returns the sum of the two lowest positive numbers
+// given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+// ✅ Solution
+function sumTwoSmallestNumbers(numbers) {
+  let num1 = Math.min(...numbers)
+  let arr1 = numbers.filter((el) => Math.min(...numbers) !== el)
+  let num2 = Math.min(...arr1)
+
+  return num1 + num2
+}
+
+console.log(sumTwoSmallestNumbers([1, 2, 3, 4])) //3
