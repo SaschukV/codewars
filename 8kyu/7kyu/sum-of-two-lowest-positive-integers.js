2f0d75074aa31c7ba3dfc2551977edd48f49ddfd
SaschukV <v.saschuk@gmail.com>
2023-11-11 21:48:00 +0200

    sumTwoSmallestNumbers1 upgrade

--- 8kyu/7kyu/sum-of-two-lowest-positive-integers.js
+++ 8kyu/7kyu/sum-of-two-lowest-positive-integers.js
@@ -7,7 +7,7 @@
 // [10, 343445353, 3453445, 3453545353453] should return 3453455.
 
 // ✅ Solution 1
-function sumTwoSmallestNumbers(numbers) {
+function sumTwoSmallestNumbers1(numbers) {
   let num1 = Math.min(...numbers)
   let arr1 = numbers.filter((el) => Math.min(...numbers) !== el)
   let num2 = Math.min(...arr1)
@@ -15,10 +15,18 @@ function sumTwoSmallestNumbers(numbers) {
   return num1 + num2
 }
 
-console.log(sumTwoSmallestNumbers([1, 2, 3, 4])) //3
+console.log(sumTwoSmallestNumbers1([1, 2, 3, 4])) //3
 
 // ✅ Solution 2
-function sumTwoSmallestNumbers(numbers) {
+function sumTwoSmallestNumbers2(numbers) {
   numbers.sort((a, b) => a - b)
   return numbers[0] + numbers[1]
 }
+
+console.log(sumTwoSmallestNumbers2([35, 6, 7, 2])) //8
+
+const sumTwoSmallestNumbers3 = (numbers) =>
+  Math.min(...numbers) +
+  Math.min(...numbers.filter((el) => Math.min(...numbers) !== el))
+
+console.log(sumTwoSmallestNumbers3([7, 8, 9, 0])) //7
