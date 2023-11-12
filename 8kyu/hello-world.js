b7620903ab79f54fe2bb18c175d43c38c57d7d2f
SaschukV <v.saschuk@gmail.com>
2023-11-12 23:40:20 +0200

    hello world

---
+++ 8kyu/hello-world.js
@@ -0,0 +1,9 @@
+// ❓DESCRIPTION:
+//Make a simple function called greet that returns the most-famous "hello world!".
+
+// ✅ Solution:
+
+function greet() {
+  return 'hello world!'
+}
+console.log(greet())
