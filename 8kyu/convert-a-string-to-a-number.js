c8900dc571c555404df66f9a02b218672c256a4c
SaschukV <v.saschuk@gmail.com>
2023-11-14 21:41:15 +0200

    stringToNumber

---
+++ 8kyu/convert-a-string-to-a-number.js
@@ -0,0 +1,10 @@
+// ❓DESCRIPTION:
+//We need a function that can transform a string into a number.
+
+// ✅ Solution1:(моё решение)
+
+const stringToNumber = function (str) {
+  return +str
+}
+
+console.log(stringToNumber('55')) // 55
