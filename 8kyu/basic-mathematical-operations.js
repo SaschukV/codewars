e1763ad3cea3e4a77b526af775325041fe28297c
SaschukV <v.saschuk@gmail.com>
2023-11-16 23:56:08 +0200

    basic-mathematical-operations

---
+++ 8kyu/basic-mathematical-operations.js
@@ -0,0 +1,25 @@
+// ❓DESCRIPTION:
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+// ✅ Solution1:(моё решение)
+function basicOp(oper, num1, num2) {
+  return oper === '+'
+    ? num1 + num2
+    : oper === '-'
+    ? num1 - num2
+    : oper === '*'
+    ? num1 * num2
+    : oper === '/'
+    ? num1 / num2
+    : undefined
+}
+console.log(basicOp('/', 1, 2)) // 0.5
