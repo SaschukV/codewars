4f6d710fb0fe3d3e4a722848b907cfeac7643ca5
SaschukV <v.saschuk@gmail.com>
2023-12-02 00:41:20 +0200

    function rent of car

---
+++ 8kyu/function-rent-of-car.js
@@ -0,0 +1,13 @@
+// ❓DESCRIPTION:
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total.
+// Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+// Write a code that gives out the total amount for different days(d).
+
+// ✅ Solution1:(моё решение)
+function rentalCarCost(d) {
+  return d >= 7 ? d * 40 - 50 : d >= 3 ? d * 40 - 20 : d * 40
+}
+console.log(rentalCarCost(8)) // 270
+console.log(rentalCarCost(2)) // 80
+console.log(rentalCarCost(5)) // 180
