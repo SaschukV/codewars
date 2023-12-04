af89491f84d72f4092d837fe16797ee124aedc74
SaschukV <v.saschuk@gmail.com>
2023-12-05 00:48:21 +0200

    remove even or uneven numbers from array Solution2

--- 7kyu/remove-even-or-uneven-number.js
+++ 7kyu/remove-even-or-uneven-number.js
@@ -34,3 +34,29 @@ console.log(
     [3, 4, 7, 8, 22],
   ])
 ) // [[2,4],[6,8,14],[4,8,22]]
+
+// ✅ Solution2:
+
+function removeEvenNumber2(arr) {
+  return arr.map((el) => el.filter((el) => el % 2 !== 0))
+}
+
+console.log(
+  removeEvenNumber2([
+    [1, 2, 3, 4, 5],
+    [13, 45, 6, 7, 8, 14],
+    [3, 4, 7, 8, 22],
+  ])
+) //[ [ 1, 3, 5 ], [ 13, 45, 7 ], [ 3, 7 ] ]
+
+function removeUnEvenNumber2(arr) {
+  return arr.map((el) => el.filter((el) => el % 2 === 0))
+}
+
+console.log(
+  removeUnEvenNumber2([
+    [1, 2, 3, 4, 5],
+    [13, 45, 6, 7, 8, 14],
+    [3, 4, 7, 8, 22],
+  ])
+) //[ [ 2, 4 ], [ 6, 8, 14 ], [ 4, 8, 22 ] ]
